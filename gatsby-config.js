1297de92a0cf65425392c5a0017c8aef78f01062
hace77 <hello@christianmartensson.se>
2024-07-04 20:31:32 +0200

    lots of changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,5 +29,16 @@ module.exports = {
       defaultDataLayer: { platform: "gatsby" }
 },
 },
+
+{
+  resolve: `gatsby-plugin-google-fonts`,
+  options: {
+    fonts: [
+      `Satisfy`,
+      `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+    ],
+    display: 'swap'
+  },
+},
   ],
 };
